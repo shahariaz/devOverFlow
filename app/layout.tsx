8c0647d8c1da3e6f93746a818a1135f5cae43c41
shahariaz <shahariaz.info@gmail.com>
2025-02-03 08:14:14 +0600

    feat: Add metaData and other icons and also minor change in clerk design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,45 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import type { Metadata } from "next";
+import { Inter, Space_Grotesk } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const SpaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+export const metadata: Metadata = {
+  title: "DevOverFlow",
+  description:
+    "A platform for developers to connect, learn, and build together",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          typography: {
+            fontFamily: `${inter.variable}, ${SpaceGrotesk.variable}`,
+          },
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang='en'>
-        <body>
-          {/* <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn> */}
+        <body className={`${inter.variable} ${SpaceGrotesk.variable}`}>
           {children}
         </body>
       </html>
